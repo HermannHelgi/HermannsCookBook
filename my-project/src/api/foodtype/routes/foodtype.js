303251d9311702de219ce3f00098fa54e69f36bd
Hermann Helgi <hermannhelgi01@gmail.com>
2024-05-29 08:13:43 +0000

    Added strapi to project

---
+++ my-project/src/api/foodtype/routes/foodtype.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foodtype router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::foodtype.foodtype');
---
+++ my-project/src/api/foodtype/services/foodtype.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foodtype service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::foodtype.foodtype');
